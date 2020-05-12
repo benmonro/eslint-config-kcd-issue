c31bb7a7bb45e9f64f54984d76e628cabc4e809e
Ben Monro <benjamin.monro@walmartlabs.com>
2020-05-12 14:52:25 -0700

    add comment

--- app/test.spec.js
+++ app/test.spec.js
@@ -3,5 +3,5 @@ import { render, screen } from '@testing-library/react';
 
 it("should render foo", () => {
     render(<div>foo</div>);
-    expect(screen.getByText("foo").getAttribute("checked")).toBe(true);
+    expect(screen.getByText("foo").getAttribute("checked")).toBe(true); // should give a lint error
 })
