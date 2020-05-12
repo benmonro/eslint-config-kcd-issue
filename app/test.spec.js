2ddd8f6de4eafb6ccfd66134448d8df57758fb07
Ben Monro <benjamin.monro@walmartlabs.com>
2020-05-12 14:51:44 -0700

    commit

---
+++ app/test.spec.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react';
+
+it("should render foo", () => {
+    render(<div>foo</div>);
+    expect(screen.getByText("foo").getAttribute("checked")).toBe(true);
+})
